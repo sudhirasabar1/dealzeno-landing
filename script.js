c63d56873ce7cc288fa36d3466f49be2c995067c
Sudhira Sam <sudhirsabar888@gmail.com>
2026-09-02 13:12:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 /*
   APNA AFFILIATE LINK YAHAN PASTE KAREIN
 */
+const AFFILIATE_LINK = "PASTE-YOUR-AFFILIATE-LINK-HERE";
+
+const TELEGRAM_LINK = "https://t.me/YOUR_CHANNEL";
+
+document.getElementById("productLink").href = AFFILIATE_LINK;
+document.getElementById("telegramLink").href = TELEGRAM_LINK;
 
 const affiliateLink = "https://bitli.in/t64sw0Q";
 
